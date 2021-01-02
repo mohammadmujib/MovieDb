4815fb587d66932b39dc3ad7be0d025d96b248d6
Mohammad Mujib <mohammadmujib09@gmail.com>
2021-01-03 01:02:41 +0530

    movideb api created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import axios from "axios";
 import "./App.css";
+import { nowPlayingUrl } from "./api/api";
 
 const queryClient = new QueryClient();
 
@@ -19,14 +21,31 @@ export default App;
 
 function Example() {
   const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=1363d7b67a206e14c8f8e2ac254a40e7&region=In&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&year=2021&with_original_language=hi"
-    ).then((res) => res.json())
+    axios.get(nowPlayingUrl()).then((res) => res.data.results)
   );
 
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
-  return <div></div>;
+  return (
+    <div>
+      {data.map((resu) => {
+        return (
+          <div>
+            <h1>{resu.original_title}</h1>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${resu.poster_path}`}
+              alt="ok"
+            />
+            <h3>{resu.overview}</h3>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${resu.backdrop_path}`}
+              alt="ok"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`;
+// const apiKey = `?${process.env.REACT_APP_API_KEY}`;
+const apiKey = "1363d7b67a206e14c8f8e2ac254a40e7";
 
-export const fetchMovies = (search = "war") =>
-  fetch(`${MOVIE_API_URL}&s=${search}`).then((response) => response.json());
+const base_url = "https://api.themoviedb.org/3/movie/";
 
-const UpcomingMovie = fetch(
-  `https://api.themoviedb.org/3/discover/movie?api_key=1363d7b67a206e14c8f8e2ac254a40e7&region=In&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&year=2021&with_original_language=hi`
-);
+const now_Playing = `now_playing?api_key=${apiKey}`;
+
+export const nowPlayingUrl = () => `${base_url}${now_Playing}`;
